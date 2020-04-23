6531567f77b0f201be299ae50487c9c8cd9ff625
bcabau1 <43051275+bcabau1@users.noreply.github.com>
2020-04-23 19:08:07 -0400

    bug fix

--- galvosimulation.py
+++ galvosimulation.py
@@ -10,34 +10,38 @@ totalx = 4.6
 totaly = totalx
 dist = 5
 
-def galvo(a, b):
-    #lx = -(float(a + totalx) - totalx)
-    #ly = (float(b + totaly) - totaly)
 
+def galvo(a, b):
     lx = -a
     ly = b
 
-    thetaX = round(math.degrees((np.arctan(lx/(2*dist)))), 4)
-    thetaY = round(math.degrees((np.arctan(ly/(2*dist)))), 4)
-    mechthetaX = thetaX/2
-    mechthetaY = thetaY/2
+    thetaX = round(math.degrees((np.arctan(lx / (2 * dist)))), 4)
+    thetaY = round(math.degrees((np.arctan(ly / (2 * dist)))), 4)
+    mechthetaX = thetaX / 2
+    mechthetaY = thetaY / 2
     dim = 2
 
     print("X mech-angle: ", mechthetaX, "X scan-angle: ", thetaX)
     print("Y mech-angle: ", mechthetaY, "Y scan-angle: ", thetaY)
 
     # Define x,y plane.
-    X, Y = np.meshgrid([-dim / 200, dim / 200], [-dim / 8, dim / 8]) #//8
+    X, Y = np.meshgrid([-dim / 200, dim / 200], [-dim / 8, dim / 8])  # //8
     Z = (4 * Y * (mechthetaY + .45)) / 200
 
     # Define inclined plane.
-    X2, Y2 = np.meshgrid([-dim / 400, dim / 400], [-dim / 4, dim / 4])  #//400
+    X2, Y2 = np.meshgrid([-dim / 400, dim / 400], [-dim / 4, dim / 4])  # //400
     Z2 = (X2 * (mechthetaX + .45))
 
     # Plot x,y plane.
-    sf = ax.plot_surface(X, Y + .5, Z + .01, color='gray', alpha=.5, linewidth=0, zorder=1)   #y mirror
+    sf = ax.plot_surface(X, Y + .5, Z + .01, color='gray', alpha=.5, linewidth=0, zorder=1)  # y mirror
     # Plot top half of inclined plane.
-    sf1 = ax.plot_surface(X2, Y2 + .5, Z2 - .01, color='blue', alpha=.5, linewidth=0, zorder=3) #x mirror
+    sf1 = ax.plot_surface(X2, Y2 + .5, Z2 - .01, color='blue', alpha=.5, linewidth=0, zorder=3)  # x mirror
+
+    sf._facecolors2d = sf._facecolors3d
+    sf._edgecolors2d = sf._edgecolors3d
+    sf1._facecolors2d = sf1._facecolors3d
+    sf1._edgecolors2d = sf1._edgecolors3d
+    ax.legend([sf, sf1], [[thetaX, '\N{DEGREE SIGN}'], [thetaY, '\N{DEGREE SIGN}']])
     return sf, sf1
 
 
@@ -62,7 +66,7 @@ for line in f:
     line = list(map(float, line))
     sf, sf1 = galvo(line[0], line[1])
     plt.plot([line[0]], dist, [line[1]], 'b,')
-    #print("X =", (line[0]), "Y =", (line[1]), "Z =", 0)
+    # print("X =", (line[0]), "Y =", (line[1]), "Z =", 0)
     fig.canvas.draw()
 
 plt.show()
