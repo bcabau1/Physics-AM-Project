19ecb6e1bdee5c2e3c0774c9c5276f289b0ce9c1
bcabau1 <43051275+bcabau1@users.noreply.github.com>
2020-04-23 16:42:40 -0400

    updated

--- galvosimulation.py
+++ galvosimulation.py
@@ -1,31 +1,68 @@
-from matplotlib import pyplot as plt
-import pylab as pl
-import scipy as sp
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
 import math
-import mpl_toolkits.mplot3d as a3
-import matplotlib.colors as colors
 
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d', proj_type='ortho')
+ax = Axes3D(fig)
+
+totalx = 4.6
+totaly = totalx
 dist = 5
 
-ax.set_xlim(-.05, .05)
+def galvo(a, b):
+    #lx = -(float(a + totalx) - totalx)
+    #ly = (float(b + totaly) - totaly)
+
+    lx = -a
+    ly = b
+
+    thetaX = round(math.degrees((np.arctan(lx/(2*dist)))), 4)
+    thetaY = round(math.degrees((np.arctan(ly/(2*dist)))), 4)
+    mechthetaX = thetaX/2
+    mechthetaY = thetaY/2
+    dim = 2
+
+    print("X mech-angle: ", mechthetaX, "X scan-angle: ", thetaX)
+    print("Y mech-angle: ", mechthetaY, "Y scan-angle: ", thetaY)
+
+    # Define x,y plane.
+    X, Y = np.meshgrid([-dim / 200, dim / 200], [-dim / 8, dim / 8]) #//8
+    Z = (4 * Y * (mechthetaY + .45)) / 200
+
+    # Define inclined plane.
+    X2, Y2 = np.meshgrid([-dim / 400, dim / 400], [-dim / 4, dim / 4])  #//400
+    Z2 = (X2 * (mechthetaX + .45))
+
+    # Plot x,y plane.
+    sf = ax.plot_surface(X, Y + .5, Z + .01, color='gray', alpha=.5, linewidth=0, zorder=1)   #y mirror
+    # Plot top half of inclined plane.
+    sf1 = ax.plot_surface(X2, Y2 + .5, Z2 - .01, color='blue', alpha=.5, linewidth=0, zorder=3) #x mirror
+    return sf, sf1
+
+
+ax.set_xlim(-.06, .06)
 ax.set_ylim(0, dist)
-ax.set_zlim(-.05, .05)
+ax.set_zlim(-.06, .06)
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
 
 file = input("Enter pattern file name: ")
 f = open(file)
-arr = [[], []]
+sf, sf1 = galvo(0, 0)
+plt.show(block=False)
 for line in f:
     if line == '':
         break
+    sf.remove()
+    sf1.remove()
     line = line.strip()
     line = line.split(';')
     line = list(map(float, line))
-    plt.plot([line[0]], 5, [line[1]], 'g.')
-    arr[0].append(line[0])
-    arr[1].append(line[1])
-    print("2HM X =", (line[0]), "Y =", (line[1]))
+    sf, sf1 = galvo(line[0], line[1])
+    plt.plot([line[0]], dist, [line[1]], 'b,')
+    #print("X =", (line[0]), "Y =", (line[1]), "Z =", 0)
     fig.canvas.draw()
 
 plt.show()
