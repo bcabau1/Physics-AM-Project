d158c3341d7a2729fa0731f2fb36e5cacfcb2eca
bcabau1 <43051275+bcabau1@users.noreply.github.com>
2020-04-23 21:44:19 -0400

    update coordinate scale, set fixed field size

--- test3d.py
+++ test3d.py
@@ -1,46 +1,55 @@
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
-import time
+import math
 
 fig = plt.figure()
 ax = Axes3D(fig)
 
-totalx = .1
+totalx = 4.6
 totaly = totalx
 dist = 5
+multiplier = 20
+
 
 def galvo(a, b):
-    lx = -(float(a + totalx)-totalx)
-    ly = (float(b + totaly)-totaly)
+    lx = -a
+    ly = b
 
-    thetaX = round((np.arctan(lx/2*dist)), 2)
-    thetaY = round((np.arctan(ly/2*dist)), 2)
-    mechthetaX = thetaX/2
-    mechthetaY = thetaY/2
+    thetaX = round(math.degrees((np.arctan(lx / (2 * dist)))), 4)
+    thetaY = round(math.degrees((np.arctan(ly / (2 * dist)))), 4)
+    mechthetaX = thetaX / 2
+    mechthetaY = thetaY / 2
     dim = 2
 
-    print("X mech-angle: ", mechthetaX*100, "X scan-angle: ", thetaX*100)
-    print("Y mech-angle: ", mechthetaY*100, "Y scan-angle: ", thetaY*100)
+    print("X mech-angle: ", mechthetaX, "X scan-angle: ", thetaX)
+    print("Y mech-angle: ", mechthetaY, "Y scan-angle: ", thetaY)
 
-    # Define x,y plane.
-    X, Y = np.meshgrid([-dim / 200, dim / 200], [-dim / 4, dim / 4])
-    Z = (4 * Y * thetaY) / 200
+    # Define y mirror
+    X, Y = np.meshgrid([-dim / 4, dim / 4], [-dim / 8, dim / 8])
+    Z = (Y * (mechthetaY/100 + .45))
 
-    # Define inclined plane.
-    X2, Y2 = np.meshgrid([-dim / 200, dim / 200], [-dim / 4, dim / 4])
-    Z2 = (X2 * thetaX)
+    # Define x mirror
+    X2, Y2 = np.meshgrid([-dim / 8, dim / 8], [-dim / 4, dim / 4])
+    Z2 = (X2 * (mechthetaX/100 + .45))
 
-    # Plot x,y plane.
-    sf = ax.plot_surface(X, Y + .5, Z + .01, color='gray', alpha=.5, linewidth=0, zorder=1)   #y mirror
-    # Plot top half of inclined plane.
-    sf1 = ax.plot_surface(X2, Y2 + .5, Z2 - .01, color='blue', alpha=.5, linewidth=0, zorder=3) #x mirror
+    # Plot y mirror.
+    sf = ax.plot_surface(X, Y + .5, Z + .5, color='gray', alpha=.5, linewidth=0, zorder=1)  # y mirror
+    # Plot x mirror
+    sf1 = ax.plot_surface(X2, Y2 + .5, Z2 - .5, color='blue', alpha=.5, linewidth=0, zorder=3)  # x mirror
+
+    sf._facecolors2d = sf._facecolors3d
+    sf._edgecolors2d = sf._edgecolors3d
+    sf1._facecolors2d = sf1._facecolors3d
+    sf1._edgecolors2d = sf1._edgecolors3d
+    ax.legend([sf, sf1], [[thetaX, '\N{DEGREE SIGN}'], [thetaY, '\N{DEGREE SIGN}']])
+
     return sf, sf1
 
 
-ax.set_xlim(-.06, .06)
+ax.set_xlim(-2.5, 2.5)
 ax.set_ylim(0, dist)
-ax.set_zlim(-.06, .06)
+ax.set_zlim(-2.5, 2.5)
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
@@ -57,9 +66,9 @@ for line in f:
     line = line.strip()
     line = line.split(';')
     line = list(map(float, line))
-    sf, sf1 = galvo(line[0], line[1])
-    plt.plot([line[0]], dist, [line[1]], 'b,')
-    #print("X =", (line[0]), "Y =", (line[1]), "Z =", 0)
+    sf, sf1 = galvo(line[0]*multiplier, line[1]*multiplier)
+    plt.plot([line[0]*multiplier], dist, [line[1]*multiplier], 'b,')
+    # print("X =", (line[0]), "Y =", (line[1]), "Z =", 0)
     fig.canvas.draw()
 
 plt.show()
