0531d473abb5dbe09c71b0dc2170654c6e9f5bdf
bcabau1 <43051275+bcabau1@users.noreply.github.com>
2020-04-24 12:50:55 -0400

    added error checking

--- test3d.py
+++ test3d.py
@@ -2,14 +2,16 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import sys
 
 fig = plt.figure()
 ax = Axes3D(fig)
 
 totalx = 4.6
 totaly = totalx
-dist = 5
+dist = 1
 multiplier = 20
+ratio = dist / 40
 
 
 def galvo(a, b):
@@ -22,29 +24,35 @@ def galvo(a, b):
     mechthetaY = thetaY / 2
     dim = 2
 
-    print("X mech-angle: ", mechthetaX, "X scan-angle: ", thetaX)
-    print("Y mech-angle: ", mechthetaY, "Y scan-angle: ", thetaY)
+    while (abs(thetaX) <= 25):
+        print("X mech-angle: ", mechthetaX, "X scan-angle: ", thetaX)
+        print("Y mech-angle: ", mechthetaY, "Y scan-angle: ", thetaY)
 
-    # Define y mirror
-    X, Y = np.meshgrid([-dim / 4, dim / 4], [-dim / 8, dim / 8])
-    Z = (Y * (mechthetaY/100 + .45))
+        # Define y mirror
+        X, Y = np.meshgrid([-dim / 4, dim / 4], [-dim * ratio, dim * ratio])
+        Z = (5 / dist * Y * (mechthetaY / 100 + .45))
+        # print(5/dist*Y)
 
-    # Define x mirror
-    X2, Y2 = np.meshgrid([-dim / 8, dim / 8], [-dim / 4, dim / 4])
-    Z2 = (X2 * (mechthetaX/100 + .45))
+        # Define x mirror
+        X2, Y2 = np.meshgrid([-dim / 8, dim / 8], [-dim * (2 * ratio), dim * (2 * ratio)])
+        Z2 = (X2 * (mechthetaX / 100 + .45))
 
-    # Plot y mirror.
-    sf = ax.plot_surface(X, Y + .5, Z + .5, color='gray', alpha=.5, linewidth=0, zorder=1)  # y mirror
-    # Plot x mirror
-    sf1 = ax.plot_surface(X2, Y2 + .5, Z2 - .5, color='blue', alpha=.5, linewidth=0, zorder=3)  # x mirror
+        # Plot y mirror.
+        sf = ax.plot_surface(X, Y + (dist * .1), Z + .5, color='gray', alpha=.5, linewidth=0, zorder=1)  # y mirror
+        # Plot x mirror
+        sf1 = ax.plot_surface(X2, Y2 + (dist * .1), Z2 - .5, color='blue', alpha=.5, linewidth=0, zorder=3)  # x mirror
 
-    sf._facecolors2d = sf._facecolors3d
-    sf._edgecolors2d = sf._edgecolors3d
-    sf1._facecolors2d = sf1._facecolors3d
-    sf1._edgecolors2d = sf1._edgecolors3d
-    ax.legend([sf, sf1], [[thetaX, '\N{DEGREE SIGN}'], [thetaY, '\N{DEGREE SIGN}']])
+        sf._facecolors2d = sf._facecolors3d
+        sf._edgecolors2d = sf._edgecolors3d
+        sf1._facecolors2d = sf1._facecolors3d
+        sf1._edgecolors2d = sf1._edgecolors3d
+        ax.legend([sf, sf1], [[thetaX, '\N{DEGREE SIGN}'], [thetaY, '\N{DEGREE SIGN}']])
 
-    return sf, sf1
+        return sf, sf1
+
+    if abs(thetaX) > 25:
+        sys.exit("Angle exceeds mechanical angle limit of +-12.5\N{DEGREE SIGN}. Please change scale of pattern or "
+                 "distance from field.")
 
 
 ax.set_xlim(-2.5, 2.5)
@@ -66,8 +74,8 @@ for line in f:
     line = line.strip()
     line = line.split(';')
     line = list(map(float, line))
-    sf, sf1 = galvo(line[0]*multiplier, line[1]*multiplier)
-    plt.plot([line[0]*multiplier], dist, [line[1]*multiplier], 'b,')
+    sf, sf1 = galvo(line[0] * multiplier, line[1] * multiplier)
+    plt.plot([line[0] * multiplier], dist, [line[1] * multiplier], 'b,')
     # print("X =", (line[0]), "Y =", (line[1]), "Z =", 0)
     fig.canvas.draw()
 
