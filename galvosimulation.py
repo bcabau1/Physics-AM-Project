bba6c96281a84240638377197e26eb3a07b4f978
bcabau1 <43051275+bcabau1@users.noreply.github.com>
2020-04-24 11:07:18 -0400

    update scale distance

--- galvosimulation.py
+++ galvosimulation.py
@@ -9,6 +9,8 @@ ax = Axes3D(fig)
 totalx = 4.6
 totaly = totalx
 dist = 5
+multiplier = 20
+ratio = dist/40
 
 
 def galvo(a, b):
@@ -24,30 +26,32 @@ def galvo(a, b):
     print("X mech-angle: ", mechthetaX, "X scan-angle: ", thetaX)
     print("Y mech-angle: ", mechthetaY, "Y scan-angle: ", thetaY)
 
-    # Define x,y plane.
-    X, Y = np.meshgrid([-dim / 200, dim / 200], [-dim / 8, dim / 8])  # //8
-    Z = (4 * Y * (mechthetaY + .45)) / 200
+    # Define y mirror
+    X, Y = np.meshgrid([-dim / 4, dim / 4], [-dim * ratio, dim * ratio])
+    Z = (5/dist * Y * (mechthetaY/100 + .45))
+    #print(5/dist*Y)
 
-    # Define inclined plane.
-    X2, Y2 = np.meshgrid([-dim / 400, dim / 400], [-dim / 4, dim / 4])  # //400
-    Z2 = (X2 * (mechthetaX + .45))
+    # Define x mirror
+    X2, Y2 = np.meshgrid([-dim / 8, dim / 8], [-dim * (2 * ratio), dim * (2 * ratio)])
+    Z2 = (X2 * (mechthetaX/100 + .45))
 
-    # Plot x,y plane.
-    sf = ax.plot_surface(X, Y + .5, Z + .01, color='gray', alpha=.5, linewidth=0, zorder=1)  # y mirror
-    # Plot top half of inclined plane.
-    sf1 = ax.plot_surface(X2, Y2 + .5, Z2 - .01, color='blue', alpha=.5, linewidth=0, zorder=3)  # x mirror
+    # Plot y mirror.
+    sf = ax.plot_surface(X, Y + (dist * .1), Z + .5, color='gray', alpha=.5, linewidth=0, zorder=1)  # y mirror
+    # Plot x mirror
+    sf1 = ax.plot_surface(X2, Y2 + (dist * .1), Z2 - .5, color='blue', alpha=.5, linewidth=0, zorder=3)  # x mirror
 
     sf._facecolors2d = sf._facecolors3d
     sf._edgecolors2d = sf._edgecolors3d
     sf1._facecolors2d = sf1._facecolors3d
     sf1._edgecolors2d = sf1._edgecolors3d
     ax.legend([sf, sf1], [[thetaX, '\N{DEGREE SIGN}'], [thetaY, '\N{DEGREE SIGN}']])
+
     return sf, sf1
 
 
-ax.set_xlim(-.06, .06)
+ax.set_xlim(-2.5, 2.5)
 ax.set_ylim(0, dist)
-ax.set_zlim(-.06, .06)
+ax.set_zlim(-2.5, 2.5)
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
@@ -64,10 +68,11 @@ for line in f:
     line = line.strip()
     line = line.split(';')
     line = list(map(float, line))
-    sf, sf1 = galvo(line[0], line[1])
-    plt.plot([line[0]], dist, [line[1]], 'b,')
+    sf, sf1 = galvo(line[0]*multiplier, line[1]*multiplier)
+    plt.plot([line[0]*multiplier], dist, [line[1]*multiplier], 'b,')
     # print("X =", (line[0]), "Y =", (line[1]), "Z =", 0)
     fig.canvas.draw()
 
 plt.show()
 f.close()
+
